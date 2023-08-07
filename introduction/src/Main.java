224071cd204a834cc9ee87810475619bdf6cc16c
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-08-07 14:02:56 +0400

    print statement for different variable

--- introduction/src/Main.java
+++ introduction/src/Main.java
@@ -12,42 +12,6 @@ public class Main {
         System.out.println(true);
         System.out.println(false);
 
-        //operation
-        System.out.println(4<5);
-        System.out.println(1!=1);
-        System.out.println(3==3);
-
-        System.out.println(20/4);
-        System.out.println(3+7);
-        System.out.println(8-3);
-        System.out.println(5*4);
-        System.out.println(5/2);
-
-        System.out.println(5.0/2);
-
-        System.out.println("     J          a           v          a   ");
-        System.out.println("    JJJ        aaa         vvv        aaa   ");
-        System.out.println("   JJJJJ      aaaaa       vvvvv      aaaaa   ");
-        System.out.println("  JJJJJJJ    aaaaaaa     vvvvvvv    aaaaaaa   ");
-        System.out.println(" JJJJJJJJJ  aaaaaaaaa   vvvvvvvvv  aaaaaaaaa   ");
-
-
-        System.out.println("   --- --- --- ---");
-        System.out.println("  |   |   |   |   |");
-        System.out.println("   --- --- --- ---");
-        System.out.println("  |   |   |   |   |");
-        System.out.println("   --- --- --- ---");
-        System.out.println("  |   |   |   |   |");
-        System.out.println("   --- --- --- ---");
-
-        System.out.println(4.0 * (1- (1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)));
-
-        System.out.println("o o o m m m m m m m ");
-        System.out.println("o o o m m m m m m m");
-        System.out.println("o o o a a a a a a a");
-        System.out.println("o o o a a a a a a a");
-        System.out.println("o o o n n n n n n n");
-        System.out.println("o o o n n n n n n n");
-
+       
     }
 }
