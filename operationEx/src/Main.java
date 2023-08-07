1b3d0c8ad01c3b5be316c7560f4287e9544bcb53
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-08-07 14:00:03 +0400

    operation example

---
+++ operationEx/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        //operation
+        System.out.println(4<5);
+        System.out.println(1!=1);
+        System.out.println(3==3);
+
+        System.out.println(20/4);
+        System.out.println(3+7);
+        System.out.println(8-3);
+        System.out.println(5*4);
+        System.out.println(5/2);
+
+        System.out.println(5.0/2);
+
+    }
+}
