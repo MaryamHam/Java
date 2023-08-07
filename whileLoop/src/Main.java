061b16ff684453665f33fb3887827d7f568ea1d9
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-08-07 20:02:05 +0400

    while loop

---
+++ whileLoop/src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        //while loop to get odd number
+        int x= 1;
+        while (x <= 100 ){
+            //
+            if(x % 2 == 1) {
+                System.out.println("odd number is " + x);
+
+            }
+            x++;
+
+        }
+
+    }
+}
