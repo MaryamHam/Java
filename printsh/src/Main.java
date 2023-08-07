275ac79f3e676b42ea41eb917bd715110826383b
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-08-07 14:04:08 +0400

    print statement

---
+++ printsh/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("     J          a           v          a   ");
+        System.out.println("    JJJ        aaa         vvv        aaa   ");
+        System.out.println("   JJJJJ      aaaaa       vvvvv      aaaaa   ");
+        System.out.println("  JJJJJJJ    aaaaaaa     vvvvvvv    aaaaaaa   ");
+        System.out.println(" JJJJJJJJJ  aaaaaaaaa   vvvvvvvvv  aaaaaaaaa   ");
+
+
+        System.out.println("   --- --- --- ---");
+        System.out.println("  |   |   |   |   |");
+        System.out.println("   --- --- --- ---");
+        System.out.println("  |   |   |   |   |");
+        System.out.println("   --- --- --- ---");
+        System.out.println("  |   |   |   |   |");
+        System.out.println("   --- --- --- ---");
+
+        System.out.println(4.0 * (1- (1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)));
+
+        System.out.println("o o o m m m m m m m ");
+        System.out.println("o o o m m m m m m m");
+        System.out.println("o o o a a a a a a a");
+        System.out.println("o o o a a a a a a a");
+        System.out.println("o o o n n n n n n n");
+        System.out.println("o o o n n n n n n n");
+
+    }
+}
