d3372082eafc7ea32f0b939b5ef6552e665d07b2
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-08-07 22:45:45 +0400

    build shape using for loop

---
+++ starFor/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+         //task 4
+        String [] star= {" *", " **"," ***"," ****"," *****"};
+        for (int i = 0; i<5; i++ ){
+
+            System.out.println(star[i]);
+        }
+    }
+}
+
